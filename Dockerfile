9f3321af29f6b8e52a95c6802c34e04cc7c2aced
commit-person <commit-person@localhost>
2018-05-01 19:54:36 +0100

    Allow Sending Logs Via UDP

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,10 @@ run dnf -y install rsyslog
 
 copy debug.conf /etc/rsyslog.d/debug.conf
 copy tcp-input.conf /etc/rsyslog.d/tcp-input.conf
+copy udp-input.conf /etc/rsyslog.d/udp-input.conf
 copy rsyslog.conf /etc/rsyslog.conf
 
 entrypoint rsyslogd -nd
 
-expose 514
+expose 514/tcp
+expose 514/udp
--- logoops.go
+++ logoops.go
@@ -6,6 +6,8 @@ import (
 	"net"
 	"os"
 	"time"
+
+	"github.com/femnad/mare"
 )
 
 const (
@@ -13,15 +15,55 @@ const (
 	defaultHost        = "localhost"
 	defaultMessage     = "hello"
 	defaultPort        = 514
+	defaultProtocol    = "tcp"
 	defaultSeverity    = 5
 	defaultTag         = "logoops"
 	expectedDateFormat = "2006-01-02 15:04:05"
 	syslogDateFormat   = "2006-01-02T15:04:05.000000-07:00"
 )
 
+var validProtocols = []string{"udp", "tcp"}
+
 func getPayload(date, hostname, tag, message string, facility, severity int) string {
 	priority := facility*8 + severity
 	return fmt.Sprintf("<%d>1 %s %s %s - - %s", priority, date, hostname, tag, message)
+}
+
+func ensureCorrectProtocol(protocol string) {
+	if !mare.Contains(validProtocols, protocol) {
+		errorMessage := fmt.Sprintf("Invalid protocol %s", protocol)
+		panic(errorMessage)
+	}
+}
+
+func parseAndFormatDate(date string) string {
+	parsedDate, err := time.Parse(expectedDateFormat, date)
+	mare.PanicIfErr(err)
+	return parsedDate.Format(syslogDateFormat)
+}
+
+func getAddress(host string, port int) string {
+	return fmt.Sprintf("%s:%d", host, port)
+}
+
+func sendPayload(protocol, address, payload string) {
+	conn, err := net.Dial(protocol, address)
+	mare.PanicIfErr(err)
+
+	fmt.Fprintf(conn, payload)
+
+	err = conn.Close()
+	mare.PanicIfErr(err)
+}
+
+func sendMessageViaSyslog(host string, port int, protocol, message, tag, hostname, date string, facility, severity int) {
+	ensureCorrectProtocol(protocol)
+
+	syslogFormattedDate := parseAndFormatDate(date)
+	address := getAddress(host, port)
+	payload := getPayload(syslogFormattedDate, hostname, tag, message, facility, severity)
+
+	sendPayload(protocol, address, payload)
 }
 
 func main() {
@@ -29,36 +71,19 @@ func main() {
 	currentDateAsString := currentDate.Format(expectedDateFormat)
 
 	currentHostname, err := os.Hostname()
-	if err != nil {
-		panic(err)
-	}
+	mare.PanicIfErr(err)
 
-	var host = flag.String("host", defaultHost, "syslog server host")
-	var port = flag.Int("port", defaultPort, "syslog server port")
-	var tag = flag.String("tag", defaultTag, "syslog tag for the log")
 	var date = flag.String("date", currentDateAsString, "date to specify for the message, with format %F %T")
-	var hostname = flag.String("hostname", currentHostname, "hostname to associate with the log")
-	var message = flag.String("message", defaultMessage, "message to send")
 	var facility = flag.Int("facility", defaultFacility, "facility for the log")
+	var hostname = flag.String("hostname", currentHostname, "hostname to associate with the log")
+	var host = flag.String("host", defaultHost, "syslog server host")
+	var message = flag.String("message", defaultMessage, "message to send")
+	var port = flag.Int("port", defaultPort, "syslog server port")
+	var protocol = flag.String("protocol", defaultProtocol, "protocol for connecting to the server")
+	var tag = flag.String("tag", defaultTag, "syslog tag for the log")
 	var severity = flag.Int("severity", defaultSeverity, "severity of the log")
+
 	flag.Parse()
 
-	parsedDate, err := time.Parse(expectedDateFormat, *date)
-	if err != nil {
-		panic(err)
-	}
-	syslogFormattedDate := parsedDate.Format(syslogDateFormat)
-
-	address := fmt.Sprintf("%s:%d", *host, *port)
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		panic(err)
-	}
-
-	payload := getPayload(syslogFormattedDate, *hostname, *tag, *message, *facility, *severity)
-	fmt.Fprintf(conn, payload)
-	err = conn.Close()
-	if err != nil {
-		panic(err)
-	}
+	sendMessageViaSyslog(*host, *port, *protocol, *message, *tag, *hostname, *date, *facility, *severity)
 }
